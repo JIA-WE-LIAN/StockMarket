6dcf85e384ee8734f2cd0059682570ec1d339886
Lian Jia Wei <lanwei125531@gmail.com>
2020-11-19 17:11:42 +0800

    '6_ngStyle'

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -9,7 +9,7 @@ import { Stock } from 'src/app/model/stock';
 export class StockItemComponent implements OnInit {
 
   public stock: Stock;
-  public stockClasses;
+  public stockStyles: any;
 
   constructor() { }
 
@@ -17,12 +17,10 @@ export class StockItemComponent implements OnInit {
     this.stock = new Stock('Test Stock Market', 'TSC', 85, 80);
     const diff = (this.stock.price / this.stock.previousPrice) - 1;
     const largeChange = Math.abs(diff) > 0.01;
-    this.stockClasses = {
-      positive: this.stock.isPositiveChange(),
-      negative: !this.stock.isPositiveChange(),
-      'large-change': largeChange,
-      'small-change': !largeChange
-    }
+    this.stockStyles = {
+      color: this.stock.isPositiveChange() ? 'green' : 'red',
+      'font-size': largeChange ? '1.2em' : '0.8em'
+    };
   }
 
   toggleFavorite() {
