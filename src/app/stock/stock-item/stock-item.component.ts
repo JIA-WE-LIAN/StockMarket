6db4480571ec881a2a53db744f32a9afed5ccec1
Lian Jia Wei <lanwei125531@gmail.com>
2020-11-20 13:42:13 +0800

    '11_projection'

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -5,22 +5,14 @@ import { Stock } from 'src/app/model/stock';
   selector: 'app-stock-item',
   templateUrl: './stock-item.component.html',
   styleUrls: ['./stock-item.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StockItemComponent {
 
   @Input() public stock: Stock;
-  @Output() private toggleFavorite: EventEmitter<Stock>;
 
-  constructor() {
-    this.toggleFavorite = new EventEmitter<Stock>();
-   }
+  constructor() { }
 
-  onToggleFavorite(event) {
-    this.toggleFavorite.emit(this.stock);
-  }
-
-  changeStockPrice() {
-    this.stock.price += 5;
+  toggleFavorite(event) {
+    this.stock.favorite = !this.stock.favorite;
   }
 }
